375c8a02bd19009c4b4bd5b5dc357aa9d136b806
demodeos <workinweb2014@ya.ru>
2021-01-20 19:38:21 +0300

    add basic functional for cookies

--- src/Cookies.php
+++ src/Cookies.php
@@ -12,12 +12,49 @@ class Cookies implements CookiesInterface
 {
     use SingleTrait;
 
-    public function get(string $name = ''): array|string
+
+
+    public function get(string $name = null): array|string
     {
-        // TODO: Implement get() method.
+        $params = $_COOKIE;
+        if($name === null){
+            return $params;
+        }
+        elseif ($name!=null && isset($params[$name])){
+            return $params[$name];
+        }
+        else{
+            return '';
+        }
     }
-    public function set(string $name, string $value, array $params = ['path' => $path])
+
+    public function set(string $name, string $value = "", int $expires = null, string $path = null, string $domain = null, bool $secure = null, bool $httponly = null): bool
     {
-        // TODO: Implement set() method.
+        $expires ??= time()+3600*24*3;
+        $path ??= '/';
+        $domain ??= $_SERVER['HTTP_HOST'];
+        $secure ??= true;
+        $httponly ??= true;
+        $name = trim($name);
+        $value = trim($value);
+        return setcookie($name, $value, $expires , $path, $domain, $secure, $httponly);
     }
+
+    public function remove($name, int $expires = null, string $path = null, string $domain = null, bool $secure = null, bool $httponly = null): bool
+    {
+        $expires ??= time()-3600*24*30;
+        $path ??= '/';
+        $domain ??= $_SERVER['HTTP_HOST'];
+        $secure ??= true;
+        $httponly ??= true;
+        $name = trim($name);
+        $value = '';
+        return setcookie($name, $value, $expires , $path, $domain, $secure, $httponly);
+    }
+
+    public function has($name, int $expires = null, string $path = null, string $domain = null, bool $secure = null, bool $httponly = null): bool
+    {
+        return isset($_COOKIE[$name]);
+    }
+
 }
