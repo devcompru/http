110fb53bf1cd99f0dcaa96c7cfa1ae87cc829710
Dmitry <workinweb2014@ya.ru>
2021-10-22 16:22:33 +0300

    relocate repo

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-0.1.0.1',
+        'version' => '0.1.0.1-dev',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f658b492a1630d246c92a15df91e6a632b873307',
+        'name' => 'devcompru/http',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'devcompru/http' => array(
+            'pretty_version' => 'dev-0.1.0.1',
+            'version' => '0.1.0.1-dev',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f658b492a1630d246c92a15df91e6a632b873307',
+            'dev_requirement' => false,
+        ),
+    ),
+);
