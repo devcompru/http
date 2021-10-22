9dec74705d48de2737aec188cc9cc580acb7e6a4
Dmitry <workinweb2014@ya.ru>
2021-10-22 14:00:09 +0300

    first commit

---
+++ src/Response.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Devcompru\Http;
+
+class Response
+{
+
+    public function __construct()
+    {
+        echo "123";
+    }
+
+
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '0.0.1.x-dev',
+        'version' => '0.0.1.9999999-dev',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e2c4262fc9ff2c453c9fb8df0b29497586e96a17',
+        'name' => 'devcompru/http',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'devcompru/http' => array(
+            'pretty_version' => '0.0.1.x-dev',
+            'version' => '0.0.1.9999999-dev',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e2c4262fc9ff2c453c9fb8df0b29497586e96a17',
+            'dev_requirement' => false,
+        ),
+    ),
+);
